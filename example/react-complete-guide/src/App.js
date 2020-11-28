8c569cc0087bfa12ed5b38560972759a3e785732
lmdung <lmdung94@gmail.com>
2020-11-28 15:34:10 +0700

    update

--- example/react-complete-guide/src/App.js
+++ example/react-complete-guide/src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: "10"},
+      {name: "Menu", age: "14"},
+      {name: "Mai", age: "30"}
+
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>hello</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
