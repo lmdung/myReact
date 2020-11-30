78073bc7bad7f2c5d41691d2936fc3976b8ad4f4
lmdung <lmdung94@gmail.com>
2020-11-30 17:22:39 +0700

    update code

--- example/react-complete-guide/src/App.js
+++ example/react-complete-guide/src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: "Max", age: "10"},
       {name: "Menu", age: "14"},
       {name: "Mai", age: "30"}
 
     ]
+  })
+  const switchNameHandler = () => {
+    console.log('was clicked')
+    setPersonsState({persons: [
+      {name: "Maximum", age: "10"},
+      {name: "Menu", age: "14"},
+      {name: "Mai", age: "15"}
+    ]})
   }
-  render() {
-    return (
-      <div className="App">
-        <h1>hello</h1>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <button onClick={switchNameHandler}>Switch name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
 }
-
-export default App;
+export default app;
